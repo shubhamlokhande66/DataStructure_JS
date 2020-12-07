347cbc82519f3bb833c93386f36249bf6ad6f607
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-07 18:31:58 +0530

    [Shubham] Adding Basic information LinkedList Program

---
+++ LinkedList/LinkedListBusinessLogic.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+/**
+ * @problem : To write a program where linked list is created and perform following operrations:
+ * 
+ * Operations: 1. AddFirst 2. addLast 3.displayLinkedList 4.search 5.Delete First 6.Delete Last 7.Exit
+ * 
+ * @author : Shubham Lokhande
+ * 
+ */
+
+//Create class Node
+//@params data take from user
