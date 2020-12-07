da3c324f1de803ac95b1a77ba3904da000d9281c
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-07 21:25:26 +0530

    [Shubham] Adding : Basic Information for Queue

---
+++ Queue/QueueBusinessLogic.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+/**
+ * @problem : To write a program where Queue is created and perform following operrations:
+ * 
+ * Operations: 1. enqueue 2. deenqueue 3.front 4.isEmpty 5.printQueue  7.Exit
+ * 
+ * @author : Shubham Lokhande
+ * 
+ */
