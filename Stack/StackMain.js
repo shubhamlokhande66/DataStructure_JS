915476cfde8a333e98f9e094f8d0658de9036a74
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-07 16:33:55 +0530

    [Shubham] Refactor : Exit case Correction

--- Stack/StackMain.js
+++ Stack/StackMain.js
@@ -1,47 +1,48 @@
-const readline = require('readline-sync');
+
+const prompt = require('prompt-sync')();
 const Stack = require('./StackBusinessLogic');
+
+
 
 //flag is taken to stop the program. i.e to come out of infinite loop
 let flag = true;
 
 //creating the object of class Stack
 let stack = new Stack();
+
 while (flag) {
 
-    //userinput to select the option in menu    
-    let userInput = readline.questionInt(`enter choice  1.push  2.pop   3.display   4. exit  : `);
+    //userunput to select the option in menu    
+    let userInput = prompt("enter choice 1.push 2.pop 3.display 4.Enter 0 Exit ");
+    console.log(userInput);
 
     switch (userInput) {
 
-        case 1:
+        case "1":
             //length of stack asks user to input stack size
-            let lengthOfStack = readline.questionInt("what is size of stack: ");
+            var lengthOfStack = Number(prompt("what is size of stack: "));
 
             if (stack.sizee() <= lengthOfStack) {
                 for (let i = 0; i < lengthOfStack; i++) {
-                    let num = readline.questionInt("Enter number to push " + i + "--> ");
-                    stack.push(num);
+                    let num = prompt("Enter number to push " + i + "--> ");
+                    stack.push(parseInt(num));
                 }
             }
             break;
 
-        case 2:
+        case "2":
             console.log("Delete the number from stack");
             console.log(stack.pop());
             break;
 
-        case 3:
+        case "3":
             console.log("elements in stack");
             console.log(stack.printStack());
             break;
 
-        case 4:
-            flag = false;
-            process.exit(1);
-            break;
-        default:
-            console.log("Provide valid option");
-            break;
-
+            default:
+                break;
+        }
+        if(userInput == 0) break;
     }
-}
+   
