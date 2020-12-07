f47fdcbda5b8e127d21ec0adc5c895e5e60c9691
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-07 15:51:02 +0530

    [Shubham] Adding : Basic requierd things

---
+++ Stack/StackMain.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+const Stack = require('./StackBusinessLogic');
+
+//flag is taken to stop the program. i.e to come out of infinite loop
+let flag = true;
+
+//creating the object of class Stack
+let stack = new Stack();
